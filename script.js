b0af3c3236e5d8e2e05ff1937896ca5df387131f
Joe Jnr <jcmaglaw@gmail.com>
2022-11-09 12:13:50 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const movieListContainer = document.getElementById('movie-list-container');
+const moviePoster = document.getElementById('movie-poster');
+const loader = document.getElementById('loader');
+// let ready = false;
+// let imagesLoaded = 0;
+// let totalImages = 0;
+let moviesArray = [];
+
+// Movie API
+const apiUrl = 'https://yts.mx/api/v2/list_movies.json?quality=3D';
+
+// Create elements forlinks and add to DOM
+function displayPosters() {
+
+  moviesArray.forEach( movie => {
+    // create anchor element to link  description page
+    const item = document.createElement('a');
+    item.setAttribute('href','./description.html');
+    item.setAttribute('target','_blank');
+
+
+    // create <img> for poster
+    const img = document.createElement('img');
+    img.setAttribute('src', movie.medium_cover_image);
+    img.setAttribute('title', movie.title);
+    img.setAttribute('alt', movie.title);
+
+    // Card text 
+    const cardText = document.createElement('div');
+    cardText.setAttribute('class', 'text_container');
+
+    const movieTitle =document.createElement('h3');
+    movieTitle.setAttribute('class', 'movie_title');
+    movieTitle.textContent=movie.title;
+
+    const movieRating =document.createElement('p');
+    movieRating.setAttribute('class', 'movie_rating');
+    movieRating.textContent=`${movie.rating}`+'☆';
+
+    // different class list dependig on rating
+    if (movie.rating >= 8){
+      movieRating.setAttribute('class', 'movie_rating high_rating');
+    }else if(movie.rating < 8 && movie.rating  >= 6){
+      movieRating.setAttribute('class', 'movie_rating moderate_rating'); 
+    }
+
+    // Put image inside <a> the both to DOM
+
+    cardText.appendChild(movieTitle);
+    cardText.appendChild(movieRating);
+
+    item.appendChild(cardText);
+    item.appendChild(img);
+    movieListContainer.appendChild(item);
+
+  });
+  
+}
+
+
+  // Get movie information from API
+async function getMovies() {
+  try {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+    const temp = result.data.movies;
+
+    moviesArray = [...temp];
+
+    console.log(moviesArray)
+    console.log(moviesArray.medium_cover_image);
+
+    displayPosters();
+
+  } catch (error) {
+    // CAtch Error
+  }
+  
+}
+
+
+getMovies();
